5c173d576587c7bacaaac683df44dcfe583b4a2f
michaelsong95@yahoo.com <michaelsong95@yahoo.com>
2025-11-22 15:18:10 -0800

    Exercise

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -1,10 +1,9 @@
-﻿namespace StaticExercise
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿using System.Threading.Channels;
+using StaticExercise;
+
+
+Console.Write($"Farenheit -> Celsius: {TempConverter.FahrenheitToCelsius(32)}");
+Console.WriteLine();
+
+Console.Write($"Celsius -> Fahrenheit : {TempConverter.CelsiusToFahrenheit(0)}");
+Console.WriteLine();
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,18 @@
+﻿namespace StaticExercise;
+
+
+
+internal static class TempConverter
+{
+    public static double FahrenheitToCelsius(double fTemp)
+    {
+        // C = (F-32) * 5 / 9;
+        return (fTemp - 32) * 5 / 9;
+    }
+
+    public static double CelsiusToFahrenheit(double cTemp)
+    {
+        // F = (C*9)/5 + 32
+        return (cTemp * 9) / 5 + 32;
+    }
+}
